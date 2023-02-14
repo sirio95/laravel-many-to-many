8b2fe5a7f423fecec1560957bda687b327a72fc0
sirio95 <sirio.iasevoli@gmail.com>
2023-02-14 17:20:58 +0100

    salvataggio preventivo corrente instabile

---
+++ database/seeders/TypologySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TypologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Typology::factory->count(25)->create();
+
+    }
+}
